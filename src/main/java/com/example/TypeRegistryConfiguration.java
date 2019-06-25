97353797d08cd0f7e9b8fd4f67e27c7894004125
Olga Saveleva <olga_saveleva@epam.com>
2019-06-25 13:02:34 +0300

    minor changes

--- src/main/java/com/example/TypeRegistryConfiguration.java
+++ src/main/java/com/example/TypeRegistryConfiguration.java
@@ -14,7 +14,7 @@ import java.util.Locale;
 import static enums.Credentials.PETER_CHAILOVSKII;
 import static java.util.Locale.ENGLISH;
 
-public class TypeRegistryConfiguration implements TypeRegistryConfigurer {
+public class TypeRegistryConfiguration implements TypeRegistryConfigurer  {
     @Override
     public void configureTypeRegistry(TypeRegistry typeRegistry) {
 
